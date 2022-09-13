bf4a4e376dcc33ea8fbd0325543a576d7306f175
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-09-13 17:24:35 +0530

    Added error logging

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const FFProbe = require("./dist/FFProbe").default;
 const FFMpeg = require("./dist/FFMpeg").default;
+
+function asJson(body, statusCode = 200) {
+    return {
+        statusCode,
+        headers: {
+            "content-type": "application/json"
+        },
+        body
+    };
+}
+
 exports.handler = async (event) => {
 
-    const {
-        rawPath,
-        queryStringParameters: {
-            url,
-        } = {},
-        headers = {},
-        body,
-    } = event;
 
-    if (rawPath.startsWith("/probe")) {
-        return await FFProbe.probe(url);
-    }
-    if (rawPath.startsWith("/thumb")) {
-        const { input, output } = JSON.parse(body);
-        return await FFMpeg.thumbnails(input, output.thumbnails);
-    }
+    try {
+        const {
+            rawPath,
+            queryStringParameters: {
+                url,
+            } = {},
+            headers = {},
+            body,
+        } = event;
 
-    return {
-        event,
-        error: "no action"
-    };
+        if (rawPath.startsWith("/probe")) {
+            return asJson(await FFProbe.probe(url));
+        }
+        if (rawPath.startsWith("/thumb")) {
+            const { input, output } = JSON.parse(body);
+            return asJson(await FFMpeg.thumbnails(input, output.thumbnails));
+        }
+
+        return asJson(event);
+    } catch (error) {
+        console.error(error);
+        return asJson(error.stack ? error.stack : error.toString(), 500);
+    }
 };
