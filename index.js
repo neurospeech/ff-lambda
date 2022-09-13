a3dee8a34192613e3aff230ee8e42364bd92254b
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-09-13 16:35:04 +0530

    Added thumbnail

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ exports.handler = async (event) => {
         rawPath,
         queryStringParameters: {
             url,
-        } = {}
+        } = {},
+        body = {},
     } = event;
 
     if (rawPath.startsWith("/probe")) {
         return await FFProbe.probe(url);
     }
-    // if (rawPath.startsWith("/thumb")) {
-    //     return await FFMpeg.thumbnails(url, thumbnails);
-    // }
+    if (rawPath.startsWith("/thumb")) {
+        return await FFMpeg.thumbnails(url, body);
+    }
 
     return {
         event,
