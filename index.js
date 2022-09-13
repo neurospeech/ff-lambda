d65fb5b636f117279cb187966e8d4b5f65c27bf0
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-09-13 17:00:41 +0530

    Parse body fixed

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ exports.handler = async (event) => {
         queryStringParameters: {
             url,
         } = {},
-        body: {
-            input,
-            output
-        } = {},
+        headers = {},
+        body,
     } = event;
 
     if (rawPath.startsWith("/probe")) {
         return await FFProbe.probe(url);
     }
     if (rawPath.startsWith("/thumb")) {
+        const { input, output } = JSON.parse(body);
         return await FFMpeg.thumbnails(input, output.thumbnails);
     }
 
