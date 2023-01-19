3cb662429955977bec65009131ca55673517bfc6
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-01-19 16:07:41 +0530

    Added copy support for m4a

--- src/FFMpeg.ts
+++ src/FFMpeg.ts
@@ -119,9 +119,16 @@ export default class FFMpeg {
             if (!existsSync(img)) {
                 console.log(`${img} does not exist !! something is wrong...`)
             }
-            // const output = await FFConfig.run(`-loop 1 -i ${img} -i ${file} -c:a aac -b:a 192k -c:v libx264 -pix_fmt yuv420p -tune stillimage -movflags +faststart -shortest ${outputFile.path}`.split(" "));
-            const output = await FFConfig.run(`-i ${file} -c:a aac -b:a 192k -movflags +faststart ${outputFile.path}`.split(" "));
-            console.log(output);
+
+            if(file.endsWith(".m4a")) {
+                const output = await FFConfig.run(`-i ${file} -c copy -movflags +faststart ${outputFile.path}`.split(" "));
+                console.log(output);
+            } else {
+
+                // const output = await FFConfig.run(`-loop 1 -i ${img} -i ${file} -c:a aac -b:a 192k -c:v libx264 -pix_fmt yuv420p -tune stillimage -movflags +faststart -shortest ${outputFile.path}`.split(" "));
+                const output = await FFConfig.run(`-i ${file} -c:a aac -b:a 192k -movflags +faststart ${outputFile.path}`.split(" "));
+                console.log(output);
+            }
         }
 
         const convert = FFMpeg.uploadFile(url, outputFile.path, true);
